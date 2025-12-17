1cadaa9698db06a25d41753f68b89e2361c76017
Tejas M <143952904+tejasm-189@users.noreply.github.com>
2025-12-17 16:47:50 +0530

    feat: add Livewire component for poll creation and integrate it into the welcome page

---
+++ resources/views/livewire/create-poll.blade.php
@@ -0,0 +1,10 @@
+<div>
+    <h1>Create a Poll</h1>
+
+    <form>
+        <label>Poll Title</label>
+        <input type="text" placeholder="Poll Title">
+
+        <button type="submit">Create Poll</button>
+    </form>
+</div>
